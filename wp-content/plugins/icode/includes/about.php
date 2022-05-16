8d38a9607a2ba3041a81cd3f73ff9fc61d5eea8d
evertonmessias <everton.messias@gmail.com>
2022-05-16 13:30:36 +0000

    16-05-2022_13:30:36

--- wp-content/plugins/icode/includes/about.php
+++
@@ -1,8 +0,0 @@
-<div class="about-icode">
-    <h1 class="title">Sobre</h1>
-    <p><b>icode</b> - é um Sistema de Gerenciamento de Conteúdos (SGC) do Conselho Interdepartamental, Congregação e Departamentos do Instituto de Computação - Unicamp..</p>    
-    <ul>
-        <li><a href="https://github.com/evertonmessias/icode" target="_blank">Projeto</a></li>
-        <li><a href="https://ic.unicamp.br/~everton" target="_blank">Site do Desenvolvedor</a></li>
-    </ul>
-</div>
